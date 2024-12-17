4031442f9b5205e87ec77a37970d4ba3ea407ed4
Hubert Jakubowski <264836@student.pwr.edu.pl>
2024-12-17 21:26:02 +0100

    Final versions

--- Lab 3/src/conv.py
+++ Lab 3/src/conv.py
@@ -2,46 +2,35 @@ import numpy as np
 from scipy.signal import convolve2d
 from numpy.typing import NDArray
 
-def apply_convolution(image: NDArray, kernel: NDArray, stride: int = 1, padding: int = 0) -> NDArray:
-    
-    if image.ndim == 2: #Greyscale image
-        edited_image = convolve2d(image, kernel, mode='same', boundary='fill', fillvalue=0)
-        return edited_image
+def apply_convolution(image: NDArray, kernel: NDArray) -> NDArray:
+    if image.ndim == 2:  
+        return convolve2d(image, kernel, mode='same')
 
-    elif image.ndim == 3 and image.shape[2] == 3:  #RGB image
+    elif image.ndim == 3 and image.shape[2] == 3: 
         edited_image = np.zeros_like(image, dtype=np.float32)
-        for channel in range(3): 
-            edited_image[:, :, channel] = convolve2d(image, kernel, mode='same', boundary='fill', fillvalue=0)
+        for channel in range(3):
+            edited_image[:, :, channel] = convolve2d(image[:, :, channel], kernel, mode='same')
         return edited_image
 
     else:
         raise ValueError("Input image must be a 2D grayscale image or a 3D RGB image with shape (height, width, 3).")
-
-
-def convolution_for_edge_finders(image: NDArray, kernels: tuple, stride: int = 1, padding: int = 0) -> NDArray:
-
     
-    plane_x, plane_y = kernels  
+def convolution_for_edge_finders(image: NDArray, kernels: tuple) -> NDArray:
+    plane_x, plane_y = kernels
 
-    if image.ndim == 2: #Greyscale image
-        
-        edges_x = convolve2d(image, plane_x, mode='same', boundary='fill', fillvalue=0)
-        edges_y = convolve2d(image, plane_y, mode='same', boundary='fill', fillvalue=0)
-        
+    if image.ndim == 2:  
+        edges_x = convolve2d(image, plane_x, mode='same')
+        edges_y = convolve2d(image, plane_y, mode='same')
         edges = np.sqrt(edges_x ** 2 + edges_y ** 2)
         return edges
 
-    elif image.ndim == 3 and image.shape[2] == 3:  #RGB image
+    elif image.ndim == 3 and image.shape[2] == 3:  
         edges = np.zeros_like(image, dtype=np.float32)
-        for channel in range(3):  
-            
-            edges_x = convolve2d(image, plane_x, mode='same', boundary='fill', fillvalue=0)
-            edges_y = convolve2d(image, plane_y, mode='same', boundary='fill', fillvalue=0)
- 
+        for channel in range(3):
+            edges_x = convolve2d(image[:, :, channel], plane_x, mode='same')
+            edges_y = convolve2d(image[:, :, channel], plane_y, mode='same')
             edges[:, :, channel] = np.sqrt(edges_x ** 2 + edges_y ** 2)
-        
         return edges
 
     else:
         raise ValueError("Input image must be a 2D grayscale image or a 3D RGB image with shape (height, width, 3).")
-
